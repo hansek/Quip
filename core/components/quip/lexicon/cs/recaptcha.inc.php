54ff3f826dc3194f546ca86e4e54b498985c622c
Jan Tezner <jan.tezner@gmail.com>
2012-01-20 12:01:30 +0000

    Preparation for translation into Czech

---
+++ core/components/quip/lexicon/cs/recaptcha.inc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Quip
+ *
+ * Copyright 2010-11 by Shaun McCormick <shaun@modx.com>
+ *
+ * This file is part of Quip, a simple commenting component for MODx Revolution.
+ *
+ * Quip is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Quip is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Quip; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package quip
+ */
+/**
+ * reCaptcha Lexicon Topic
+ *
+ * @package quip
+ * @subpackage lexicon
+ *
+ * @author modxcms.cz
+ * @updated 2011-04-24
+ */
+$_lang['recaptcha.empty_answer'] = 'Nesprávná captcha slova. Zkontrolujte svou odpověď a zkuste to znovu.';
+$_lang['recaptcha.incorrect'] = 'reCAPTCHA nebyla zadána správně. Jděte zpět a zkuste to znovu. [[+error]]';
+$_lang['recaptcha.mailhide_no_mcrypt'] = 'Pro využívání reCAPTCHA Mailhide musíte mít nainstalovaný PHP modul "mcrypt".';
+$_lang['recaptcha.mailhide_no_api_key'] = 'Pro využívání reCAPTCHA Mailhide se musíte zaregistrovat a tak získat veřejný a soukromý klíč, můžete tak učinit na adrese <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
+$_lang['recaptcha.no_api_key'] = 'Pro využívání reCAPTCHA musíte získat klíč API na adrese <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
+$_lang['recaptcha.no_remote_ip'] = 'Z bezpečnostních důvodů, musíte zadat vzdálenou IP do reCAPTCHA';
+
